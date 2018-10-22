e186b886b1d4f54bf4a54bf211929352f2539eaf
Jerry <mikeynjerry@gmail.com>
2018-10-22 15:32:29 -0400

    Unauthenticated Redirect and 404 Page

---
+++ react-ui/src/Lost.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import slide from './VCLost.svg';
+import './VocaCoord.css';
+
+export default class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <div className="App-homepage">
+          <img src={slide} className="App-slide" alt="slide" />
+        </div>
+      </div>
+    );
+  }
+}
--- react-ui/src/Routing.js
+++ react-ui/src/Routing.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './Header.js';
 import Homepage from './Homepage.js';
 import Login from './Login.js';
 import Signup from './Signup.js';
 import Classrooms from './Classrooms.js';
+import Lost from './Lost.js';
 import { WordBanks, WordBank } from './WordBanks.js';
+import PrivateRoute from './PrivateRoute.js';
 import './VocaCoord.css';
 
 export default class Routing extends Component {
@@ -14,12 +16,15 @@ export default class Routing extends Component {
       <Router>
         <div className="App">
           <Header />
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/classrooms" component={Classrooms} />
-          <Route exact path="/classrooms/:classroom/wordbanks" component={WordBanks} />
-          <Route exact path="/classrooms/:classroom/wordbanks/words" component={WordBank} />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <PrivateRoute exact path="/classrooms" component={Classrooms} />
+            <PrivateRoute exact path="/classrooms/:classroom/wordbanks" component={WordBanks} />
+            <PrivateRoute exact path="/classrooms/:classroom/wordbanks/words" component={WordBank} />
+            <Route component={Lost} />
+          </Switch>
         </div>
       </Router>
     )
