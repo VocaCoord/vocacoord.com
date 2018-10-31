069b67a8b17c839ca31ea2d9ad91f4b53cbe869b
Jerry <mikeynjerry@gmail.com>
2018-10-31 00:10:05 -0400

    Remove React Redux Sessions
    Delay Rendering App Until Auth Check

--- website/src/index.js
+++ website/src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routing from "./Routing.js";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
-import { sessionService } from 'redux-react-session';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 const store = configureStore();
-
-sessionService.initSessionService(store);
 
 ReactDOM.render(
   <Provider store={store}>
