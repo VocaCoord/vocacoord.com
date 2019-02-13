ea8f5fd73b86c5762d000eb2879eda1cacefca9e
Jerry <MikeynJerry@gmail.com>
2019-02-13 14:55:37 -0500

    Changed Project to React Firebase

--- src/components/Homepage/Homepage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import slide from '../../assets/VCSlide.svg';
-import '../../VocaCoord.css';
-
-const Homepage = () => <img src={slide} className="homepage" alt="slide" />;
-
-export default Homepage;
--- src/containers/WordBanks/index.js
+++
@@ -1,1 +0,0 @@
-export { default as WordBanks } from './WordBanks';
